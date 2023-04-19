e203e1001655ca11699e6ac0b5c4503580a80bfb
Ahmed Hesham <96313662+AH-HESHAM@users.noreply.github.com>
2023-04-19 13:34:24 +0200

    floyed (parents are not checked yet)

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -1,16 +1,10 @@
 import java.util.*;
 
 public class GraphProcessor {
-     static Graph graph;
-     int source;
-     int[] costs;
-     int[] parents;
+    static Graph graph;
 
     public GraphProcessor(Graph graph, int source, int[] costs, int[] parents) {
         this.graph = graph;
-        this.source = source;
-        this.costs = costs;
-        this.parents = parents;
     }
 
     public static void dijkstra(int source, int[] costs, int[] parents) {
@@ -22,7 +16,8 @@ public class GraphProcessor {
         while (!pq.isEmpty()) {
             Node node = pq.poll();
             int u = node.vertex;
-            if (node.cost > costs[u]) continue; // skip if not improving
+            if (node.cost > costs[u])
+                continue; // skip if not improving
             for (Graph.Edge e : graph.getAdj(u)) {
                 int v = e.getV();
                 int newCost = costs[u] + e.weight;
@@ -78,8 +73,36 @@ public class GraphProcessor {
         return true; // no negative-weight cycle found
     }
 
-
-
-
+    public static boolean floyedWarsell(int[][] costs, int[][] parents) {
+        int n = graph.getV();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                costs[i][j] = Integer.MAX_VALUE;
+                parents[i][j] = -1;
+            }
+        }
+        for (int i = 0; i < n; i++)
+            costs[i][i] = 0;
+        for (int i = 0; i < n; i++) {
+            for (Graph.Edge e : graph.getEdges(i)) {
+                costs[e.getU()][e.getV()] = e.getWeight();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (j != i && k != i && costs[j][i] != Integer.MAX_VALUE && costs[i][k] != Integer.MAX_VALUE) {
+                        costs[j][k] = Math.min(costs[j][k], costs[j][i] + costs[i][k]);
+                        parents[j][k] = i;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (costs[i][i] < 0)
+                return false;
+        }
+        return true;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,9 @@ import java.util.Scanner;
 
 import static java.lang.System.exit;
 
-
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        String path = "/home/abdelrahman/Desktop/ShortPathTest.pl";
+        String path = "E:/level2/DS2/test.txt";
         File file = new File(path);
 
         Graph graph = Graph.Initialize(file);
@@ -26,29 +25,43 @@ public class Main {
         int[] parents = new int[graph.getV()];
 
         GraphProcessor gp = new GraphProcessor(graph, source, costs, parents);
-        if(userInput ==  1) {
+        if (userInput == 1) {
             GraphProcessor.dijkstra(source, costs, parents);
         } else if (userInput == 2) {
             boolean noCycle;
             noCycle = GraphProcessor.bellmanFord(source, costs, parents);
-            if(noCycle){
+            if (noCycle) {
                 System.out.println("No negative Cycles \n");
-            }else {
+            } else {
                 System.out.println("Cycle found \n");
             }
-        } else if(userInput == 3){
-            //Floyd warshall
-        } else{
+        } else if (userInput == 3) {
+            boolean noCycle;
+            int[][] p = new int[graph.getV()][graph.getV()];
+            int[][] c = new int[graph.getV()][graph.getV()];
+            noCycle = GraphProcessor.floyedWarsell(c, p);
+            if (noCycle) {
+                System.out.println("No negative Cycles \n");
+            } else {
+                System.out.println("Cycle found \n");
+            }
+            for (int i = 0; i < graph.getV(); i++) {
+                for (int j = 0; j < graph.getV(); j++) {
+                    System.out.print(c[i][j] + " ");
+                }
+                System.out.println();
+
+            }
+        } else {
             System.out.println("Wrong input");
             exit(1);
         }
 
-
-//        //print graph
-//        int v = graph.getV();
-//        for(int i = 0; i < v; i++){
-//            System.out.println(graph.getAdj(i));
-//        }
+        // //print graph
+        // int v = graph.getV();
+        // for(int i = 0; i < v; i++){
+        // System.out.println(graph.getAdj(i));
+        // }
 
     }
 }
